0550b76a6280d96eb7c766d85d179542fe0fba43
Logan <lgntravis@gmail.com>
2023-06-09 23:04:25 -0600

    no crashing if env file doesn't exist

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func main() {
 
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("Error loading .env file:", err)
+		fmt.Println("Error loading .env file:", err)
 	}
 
 	value := os.Getenv("DISCORD_TOKEN")
