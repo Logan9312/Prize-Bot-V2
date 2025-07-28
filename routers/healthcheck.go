3366c41dd2a6c2dccaa5eefa821e896e7a8bbefd
Logan Travis <lgntravis@gmail.com>
2025-07-28 16:05:47 -0600

    Format

--- routers/healthcheck.go
+++ routers/healthcheck.go
@@ -7,7 +7,7 @@ import (
 func HealthCheck(app *fiber.App) {
 	app.Get("/health", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
-			"status": "ok",
+			"status":  "ok",
 			"message": "Server is healthy",
 		})
 	})
