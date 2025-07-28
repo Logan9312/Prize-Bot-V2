ef6aacb5269910efb9850f1cbf0ff614ac6723fc
Logan Travis <lgntravis@gmail.com>
2025-07-28 16:51:08 -0600

    Fixing app run

--- routers/base.go
+++ routers/base.go
@@ -1,11 +1,22 @@
 package routers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func StartApp() {
 	app := fiber.New()
 
 	healthCheck(app)
 
-	app.Listen(":3000")
+	// Get port from environment or default to 3000
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	log.Fatal(app.Listen("[::]:" + port))
 }
