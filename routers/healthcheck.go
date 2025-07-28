1598d3ac5893fc6827abc1e05c6d198ced3d0c04
Logan Travis <lgntravis@gmail.com>
2025-07-28 16:19:11 -0600

    c.JSON

--- routers/healthcheck.go
+++ routers/healthcheck.go
@@ -6,7 +6,7 @@ import (
 
 func HealthCheck(app *fiber.App) {
 	app.Get("/health", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(fiber.Map{
+		return c.JSON(fiber.Map{
 			"status":  "ok",
 			"message": "Server is healthy",
 		})
